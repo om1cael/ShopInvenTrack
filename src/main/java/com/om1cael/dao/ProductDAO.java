ca25bd2b59ab1aaba43a7e992772ecee71b04bd9
om1cael <188113439+om1cael@users.noreply.github.com>
2024-12-20 21:40:42 -0300

    feat: setup get all products method

--- src/main/java/com/om1cael/dao/ProductDAO.java
+++ src/main/java/com/om1cael/dao/ProductDAO.java
@@ -4,8 +4,10 @@ import com.om1cael.model.Product;
 import com.om1cael.utils.DBConnectionProvider;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProductDAO {
@@ -16,6 +18,31 @@ public class ProductDAO {
             this.connection = dbConnectionProvider.getDBConnection();
         } catch (SQLException e) {
             throw new RuntimeException("Failed to connect to the database: " + e.getSQLState());
+        }
+    }
+
+
+
+    public List<Product> getAllProducts() {
+        try(Statement queryAllStatement = connection.createStatement()) {
+            ResultSet resultSet = queryAllStatement.executeQuery("SELECT * FROM shop");
+            List<Product> productList = new ArrayList<>();
+
+            while(resultSet.next()) {
+                Product product = new Product(
+                        resultSet.getInt("id"),
+                        resultSet.getString("name"),
+                        resultSet.getString("description"),
+                        resultSet.getDouble("price"),
+                        resultSet.getInt("stock")
+                );
+
+                productList.add(product);
+            }
+
+            return productList;
+        } catch (SQLException e) {
+            throw new RuntimeException("It was not possible to query all products: " + e.getSQLState());
         }
     }
 }
--- src/main/java/com/om1cael/model/Product.java
+++ src/main/java/com/om1cael/model/Product.java
@@ -1,3 +1,3 @@
 package com.om1cael.model;
 
-public record Product(int id, String name, String description, double price, int quantity) { }
+public record Product(int id, String name, String description, double price, int stock) { }
